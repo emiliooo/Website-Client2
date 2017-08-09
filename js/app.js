66b716dbf5249135ff5fc83be0795b0025aa6eca
DESKTOP-4TAVHA0\Emil007 <emilburaczynski@gmail.com>
2017-08-09 11:19:46 +0200

    2.3

--- js/app.js
+++ js/app.js
@@ -15,47 +15,48 @@ $( document ).ready(function() {
 		$('.add_menu').addClass('added');
     } else {
         $('.add_menu').removeClass('added');
-    }
+	}
+	
 	});
 
 		$("#onet").click(function() {
 			$('html, body').animate({
 		scrollTop: $("#section1").offset().top
 			}, 2000);
-	    $("#menuID").css("display","none")
+	    
 	});
 
 		$("#twot").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#section2").offset().top
 		}, 2000);
-		$("#menuID").css("display","none")
+		
 	});
 
 		$("#three").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#section3").offset().top - 20 
 		}, 2000);
-		$("#menuID").css("display","none")
+		
 	});
 
 		$("#five").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#section4").offset().top - 100
 		}, 2000);
-		$("#menuID").css("display","none")
+		
 	});
 		$("#six").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#section6").offset().top - 40
 		}, 2000);
-		$("#menuID").css("display","none")
+		
 	});
 		$("#seven").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#section7").offset().top -140
 		}, 2000);
-		$("#menuID").css("display","none")
+		
 	});
 
 
